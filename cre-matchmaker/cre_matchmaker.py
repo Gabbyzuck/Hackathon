66c09a07f73378f98ea317bb81ecafebbdb63217
Gabby <gabbyzuck@gmail.com>
2025-04-27 18:42:26 +0300

    Added investor profile function

--- cre-matchmaker/cre_matchmaker.py
+++ cre-matchmaker/cre_matchmaker.py
@@ -1,6 +1,11 @@
 import json
 
-with open('data/properties.json') as f:
-    properties = json.load(f)
+def load_json(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
 
-print(properties)
+if __name__ == "__main__":
+    data = load_json('data/properties.json')
+    for property in data:
+        print(property)
